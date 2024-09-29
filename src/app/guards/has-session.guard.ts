a62fe7a92e6f6785b56019ef1c4ebecc4120b91d
Tarcio Diniz <tarciodiniz0@gmail.com>
2024-09-29 10:17:35 -0300

    fix: resolve guard de rota

--- src/app/guards/has-session.guard.ts
+++ src/app/guards/has-session.guard.ts
@@ -1,19 +1,25 @@
-import { inject } from '@angular/core';
-import { CanActivateFn, Router } from '@angular/router';
-import { SessionService } from '@store/session.service';
-import { catchError, map, of } from 'rxjs';
+import {inject} from '@angular/core';
+import {CanActivateFn, Router} from '@angular/router';
+import {SessionService} from '@store/session.service';
+import {of} from 'rxjs';
+import {first, map} from 'rxjs/operators';
 
 export const hasSessionGuard: CanActivateFn = (route, state) => {
-
   const router = inject(Router);
   const sessionService = inject(SessionService);
 
-  if(!sessionService.isAuthenticated()) {
-    router.navigate(['/login']);
-    return of(false);
-  }
-  else {
-    return of(true);
+  if (sessionService.isSessionLoading) {
+    return of(false); // ou mostrar um loading
   }
 
+  return sessionService.getUser().pipe(
+    first(),
+    map(user => {
+      if (!sessionService.isAuthenticated()) {
+        router.navigate(['/login']).then();
+        return false;
+      }
+      return true;
+    })
+  );
 };
--- src/app/store/session.service.ts
+++ src/app/store/session.service.ts
@@ -1,36 +1,47 @@
-import { Injectable } from "@angular/core";
-import { SessionStore } from "./session.store";
-import { Router } from "@angular/router";
-import { ToastrService } from "ngx-toastr";
-import { tap, map, take, mergeMap, shareReplay } from 'rxjs/operators';
-import { Observable, of } from "rxjs";
-import { applyTransaction, resetStores } from "@datorama/akita";
-import { AuthService } from "@services/auth.service";
-import { LocalStorageService } from "@services/local-storage.service";
-import { User } from "@models/user";
-import { UserService } from "@services/user.service";
-import { SessionQuery } from "./session.query";
-import { firstValueFrom } from 'rxjs';
+import {Injectable} from "@angular/core";
+import {SessionStore} from "./session.store";
+import {Router} from "@angular/router";
+import {ToastrService} from "ngx-toastr";
+import {map, mergeMap, shareReplay, take, tap} from 'rxjs/operators';
+import {firstValueFrom, Observable, of} from "rxjs";
+import {applyTransaction, resetStores} from "@datorama/akita";
+import {AuthService} from "@services/auth.service";
+import {LocalStorageService} from "@services/local-storage.service";
+import {User} from "@models/user";
+import {UserService} from "@services/user.service";
 
 @Injectable({
-	providedIn: 'root'
+  providedIn: 'root'
 })
 export class SessionService {
-	constructor(
-		private readonly _sessionStore: SessionStore,
-		private readonly _router: Router,
-		private readonly _toastr: ToastrService,
+  private isLoading = false;
+
+  constructor(
+    private readonly _sessionStore: SessionStore,
+    private readonly _router: Router,
+    private readonly _toastr: ToastrService,
     private readonly _authService: AuthService,
-		private readonly _userService: UserService,
-		private readonly _storage: LocalStorageService,
-	) {
+    private readonly _userService: UserService,
+    private readonly _storage: LocalStorageService,
+  ) {
 
     this.getUserFromBack();
   }
 
+  public loadUserFromBack(): void {
+    this.isLoading = true;
+    this.getUserFromBack().subscribe(() => {
+      this.isLoading = false;
+    });
+  }
+
+  public get isSessionLoading(): boolean {
+    return this.isLoading;
+  }
+
   public login(email: string, password: string): Promise<any> {
     return firstValueFrom(
-      this._authService.login({ email, password }).pipe(take(1))
+      this._authService.login({email, password}).pipe(take(1))
     ).then(
       (res) => {
         this.handleLoginResponse(res);
@@ -43,11 +54,11 @@ export class SessionService {
     );
   }
 
-	public logout() {
+  public logout() {
 
-		this._authService.logout()
-			.pipe(take(1))
-			.subscribe({
+    this._authService.logout()
+      .pipe(take(1))
+      .subscribe({
         next: () => {
           this.updateIsAuthenticated(false);
           this._router.navigate(['/login']);
@@ -55,63 +66,60 @@ export class SessionService {
         error: (err) => {
         }
       });
-		resetStores();
-		this._storage.clear();
-		this.updateIsAuthenticated(false);
-		this._router.navigate(['/login']);
-	}
+    resetStores();
+    this._storage.clear();
+    this.updateIsAuthenticated(false);
+    this._router.navigate(['/login']);
+  }
 
 
-	public handleLoginResponse(response: any): void {
+  public handleLoginResponse(response: any): void {
     this._storage.set('name', response.name);
     this._storage.set('user_id', response.user.id);
     this._storage.set('access_token', response.access_token);
-    if(response.type === 'MASTER') {
-      this._sessionStore.update({ isMaster: true });
-    };
+    if (response.type === 'MASTER') {
+      this._sessionStore.update({isMaster: true});
+    }
+    ;
     this.updateIsAuthenticated(true);
-	}
+  }
 
-	public handleLoginError(err: any): void {
-		this._toastr.warning(err.error.error);
-	}
+  public handleLoginError(err: any): void {
+    this._toastr.warning(err.error.error);
+  }
 
-	public getUser(): Observable<User> {
+  public getUser(): Observable<User> {
     return this._sessionStore
       ._select(state => state.user)
       .pipe(
-          mergeMap(user => {
-              if (user) {
-                  return of(user);
-              }
-              return this.getUserFromBack();
-          })
+        mergeMap(user => {
+          if (user) {
+            return of(user);
+          }
+          return this.getUserFromBack();
+        })
       );
   }
 
   public getUserFromBack(): Observable<User> {
-      return this._userService.getUser()
-          .pipe(
-              map(user => user.data),
-              tap(user => {
-                this._sessionStore.update({ user });
-              }),
-              shareReplay()
-          )
+    return this._userService.getUser()
+      .pipe(
+        map(user => user.data),
+        tap(user => {
+          this._sessionStore.update({user});
+        }),
+        shareReplay()
+      )
   }
 
   public updateIsAuthenticated(isAuthenticated: boolean) {
     applyTransaction(() => {
       this._sessionStore.setLoading(false);
-      this._sessionStore.update({ isAuthenticated });
+      this._sessionStore.update({isAuthenticated});
     });
   }
 
   public isAuthenticated(): boolean {
-    if(this._storage.get('access_token'))
-      return true;
-    else
-      return false;
+    return !!this._storage.get('access_token');
   }
-
 }
