19120a2b10ed403d6164d3c30b4dc00464524bc5
emilsonsn <emilsonsn2@gmail.com>
2024-11-05 14:44:55 -0300

    "version": "1.0.4"

--- src/environments/environment.dev.ts
+++ src/environments/environment.dev.ts
@@ -5,6 +5,7 @@ export const environment = {
   appName: 'Granatum App',
   home: '/painel',
   api: 'http://127.0.0.1:8000/api',
+  // api: 'https://api.primewebsistema.com.br/api',
   version: require('../../package.json').version
 };
 
